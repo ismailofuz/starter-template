aec654facad6d9dfcc2d8cdb0c8e510d66d1aca0
ismailofuz <sirojiddin.ismoilov7181@gmail.com>
2024-02-08 09:14:23 +0500

    linting

--- src/repository/classes/users.ts
+++ src/repository/classes/users.ts
@@ -10,23 +10,43 @@ import UsersRepositoryInterface, {
 @Injectable()
 export class UsersRepository implements UsersRepositoryInterface {
     constructor(@InjectKnex() private readonly knex: Knex) {}
-    create(dto: CreateUser): Promise<UserI> {
-        throw new Error('Method not implemented.');
-    }
-    findById(id: number): Promise<UserI> {
-        throw new Error('Method not implemented.');
-    }
-    find(query: QueryUsers): Promise<UserI[]> {
-        throw new Error('Method not implemented.');
-    }
-    update(id: number, dto: Partial<Omit<UserI, 'id'>>): Promise<UserI> {
-        throw new Error('Method not implemented.');
-    }
-    delete(id: number): Promise<UserI> {
-        throw new Error('Method not implemented.');
-    }
 
     private get users() {
         return this.knex<UserI>('users');
+    }
+
+    async create(dto: CreateUser): Promise<UserI> {
+        return this.users
+            .insert(dto)
+            .returning('*')
+            .then((rows) => rows[0]);
+    }
+
+    findById(id: number): Promise<UserI> {
+        return this.users.where({ id }).first();
+    }
+
+    find(query: QueryUsers): Promise<UserI[]> {
+        return this.users
+            .where('name', 'ilike', `%${query.q}%`)
+            .orWhere('email', 'ilike', `%${query.q}%`)
+            .limit(query.page.limit)
+            .offset(query.page.offset);
+    }
+
+    async update(id: number, dto: Partial<Omit<UserI, 'id'>>): Promise<UserI> {
+        return this.users
+            .where({ id })
+            .update(dto)
+            .returning('*')
+            .then((rows) => rows[0]);
+    }
+
+    async delete(id: number): Promise<UserI> {
+        return this.users
+            .where({ id })
+            .del()
+            .returning('*')
+            .then((rows) => rows[0]);
     }
 }
